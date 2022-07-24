221aa15cb70466e4e15ad3b7e861f2e69146be51
ExiledVegan <exiledvegan@gmail.com>
2022-07-24 15:56:48 +0300

    Fixed 404 and thanks.js pages + a small bug in the style.scss file

--- src/pages/404.js
+++ src/pages/404.js
@@ -23,8 +23,8 @@ const NotFound = () => (
         />
         <h1>Oops we did not expect that to happen</h1>
         <p>
-          Have you wondered into the unknow. Let us help you, Please take a look
-          at below options
+          Have you wandered into the unknown? Let me help you, please take a look
+          at the options below
         </p>
       </header>
       <Link to="/" className="button">
